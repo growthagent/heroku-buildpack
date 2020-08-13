cb32b41681372a9d5e9777857cbc8075002f7a3a
toastal <toastal@protonmail.com>
2020-08-13 22:03:39 +0700

    dry

---
+++ lib/asdf-plugins.sh
@@ -0,0 +1,14 @@
+add_install_asdf_plugin() {
+  local plugin="$1"
+  local repo=${2:-}
+  if ! asdf plugin list | grep $plugin; then
+    echo "-----> Adding asdf $plugin"
+    asdf plugin add \$plugin $repo
+  else
+    echo "-----> Updating asdf plugin $plugin"
+    asdf plugin update $plugin
+  fi
+}
+
+add_install_asdf_plugin "dhall"
+add_install_asdf_plugin "purescript-zephyr" "https://github.com/instateam/asdf-purescript-zephyr.git"
